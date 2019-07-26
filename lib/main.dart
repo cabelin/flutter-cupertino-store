d50badd837d79a03276dec655c97d74c9a68a1f4
Mateus Moraes <mmds89@gmail.com>
2019-07-26 00:00:50 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import 'app.dart';
+import 'model/app_state_model.dart'; 
+
+void main() {
+  
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      builder: (context) => AppStateModel()..loadProducts(),
+      child: CupertinoStoreApp(),
+    )
+  );
+}
